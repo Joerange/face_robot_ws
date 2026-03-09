89838cfb055d643486eecd51c0bb2f3e97099ad0
Joe Range <qiaoyunzhi1@163.com>
2026-03-09 20:48:05 +0800

    Stop tracking ROS build artifacts

--- build/face_robot_driver/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/tuf/Mark/face_robot_ws/install/face_robot_driver'
--- build/face_robot_driver/share/face_robot_driver/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/tuf/Mark/face_robot_ws/build/face_robot_driver"
